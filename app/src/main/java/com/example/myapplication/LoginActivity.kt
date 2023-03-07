df5636b7516d74a6c0355656e8c9caff2dab5316
Allen Chen <allenc456123@gmail.com>
2023-03-07 17:54:08 -0500

    Changed root module name

--- app/src/main/java/com/example/myapplication/LoginActivity.kt
+++ app/src/main/java/com/example/myapplication/LoginActivity.kt
@@ -2,12 +2,21 @@ package com.example.myapplication
 
 import android.content.Intent
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.spotify.android.appremote.api.ConnectionParams
+import com.spotify.android.appremote.api.Connector
+import com.spotify.android.appremote.api.SpotifyAppRemote
+
+
+private const val CLIENT_ID = "8a9c4c8a356e484eb01e57b844806133"
+private const val REDIRECT_URI = "http://example.com/callback/"
+private var mSpotifyAppRemote: SpotifyAppRemote? = null
 
 class LoginActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
@@ -37,6 +46,27 @@ class LoginActivity : AppCompatActivity() {
 
     override fun onStart() {
         super.onStart()
+        val connectionParams = ConnectionParams.Builder(CLIENT_ID)
+            .setRedirectUri(REDIRECT_URI)
+            .showAuthView(true)
+            .build()
+        SpotifyAppRemote.connect(this, connectionParams,
+            object : Connector.ConnectionListener {
+                override fun onConnected(spotifyAppRemote: SpotifyAppRemote) {
+                    mSpotifyAppRemote = spotifyAppRemote
+                    Log.d("MainActivity", "Connected! Yay!")
+
+                    // Now you can start interacting with App Remote
+                    //connected()
+                }
+
+                override fun onFailure(throwable: Throwable) {
+                    Log.e("MainActivity", throwable.message, throwable)
+
+                    // Something went wrong when attempting to connect! Handle errors here
+                }
+            })
+
         Log.i("myTag","OnStart for Login Activity has been called")
     }
 
