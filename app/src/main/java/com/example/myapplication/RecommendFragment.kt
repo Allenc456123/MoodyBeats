a3d1b3fdaca5c5b72f948e7397e33059556d2c00
Bkfinley8 <brennan8192@gmail.com>
2023-03-29 02:32:59 -0400

    Fixed light sensor

--- app/src/main/java/com/example/myapplication/RecommendFragment.kt
+++ app/src/main/java/com/example/myapplication/RecommendFragment.kt
@@ -10,7 +10,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import androidx.core.content.ContextCompat.getSystemService
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.LinkedBlockingQueue
 
 
 /**
@@ -19,6 +23,12 @@ import androidx.core.content.ContextCompat.getSystemService
  * create an instance of this fragment.
  */
 class RecommendFragment : Fragment() {
+
+    private lateinit var mBtnCheckLight: Button
+    private lateinit var mtvRecommendFragment: TextView
+
+    private lateinit var sensorManager: SensorManager
+    private lateinit var lightSensor: Sensor
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,24 +40,69 @@ class RecommendFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         // Here you can access your views and add your logic
+        mBtnCheckLight = view.findViewById<Button>(R.id.btnLightCheck)
+        mtvRecommendFragment = view.findViewById<TextView>(R.id.tvRecommendFragment)
 
-    }
+        sensorManager = requireContext().getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT)
 
-    private fun measureAmbientLight(): Float {
-        val sensorManager = requireContext().getSystemService(Context.SENSOR_SERVICE) as SensorManager
-        val lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT)
-        var ambientLightValue: Float = -1.00f
-        val lightSensorListener = object : SensorEventListener {
-            override fun onSensorChanged(event: SensorEvent?) {
-                if (event != null) {
-                    ambientLightValue = event.values[0]
-                    sensorManager.unregisterListener(this)
-                }
-            }
-            override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {}
+        mBtnCheckLight.setOnClickListener {
+            var measure = measureAmbientLight()
+            mtvRecommendFragment.text = measure.toString()
         }
-        sensorManager.registerListener(lightSensorListener, lightSensor, SensorManager.SENSOR_DELAY_NORMAL)
-        return ambientLightValue
+
+
     }
+
+//    fun measureAmbientLight(context: Context): Float {
+//        val sensorManager = context.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+//        val lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT)
+//        var lightValue = -1.0f
+//
+//        val sensorListener = object : SensorEventListener {
+//            override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {}
+//
+//            override fun onSensorChanged(event: SensorEvent?) {
+//                if (event?.sensor?.type == Sensor.TYPE_LIGHT) {
+//                    lightValue = event.values[0]
+//                    sensorManager.unregisterListener(this)
+//                }
+//            }
+//        }
+//
+//        sensorManager.registerListener(sensorListener, lightSensor, SensorManager.SENSOR_DELAY_NORMAL)
+//
+//        // Wait for the sensor to return a value
+//        while (lightValue < 0.0f) {
+//            Thread.sleep(50)
+//        }
+//
+//        return lightValue
+//    }
+
+    private fun measureAmbientLight() {
+        sensorManager.registerListener(sensorListener, lightSensor, SensorManager.SENSOR_DELAY_NORMAL)
+    }
+
+    private val sensorListener = object : SensorEventListener {
+        override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {}
+
+        override fun onSensorChanged(event: SensorEvent?) {
+            if (event?.sensor?.type == Sensor.TYPE_LIGHT) {
+                val lightValue = event.values[0]
+                mtvRecommendFragment.text = lightValue.toString()
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        sensorManager.unregisterListener(sensorListener)
+    }
+
+
+
+
+
 
 }
